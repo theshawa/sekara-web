b991c9bfecfa0b8a65e7d27b8fa58ed611d8636b
rowneth <wadroneth@gmail.com>
2024-08-06 14:05:14 +0530

    test

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import * as Colors from "tailwindcss/colors";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    colors: {
+      slate: Colors.slate,
+      brand: { dark: Colors.cyan[500], DEFAULT: Colors.cyan[400] },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
