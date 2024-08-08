fea624c96447a2d7bbba3a0a27317883ae25cc98
mr-chamika <chamikauni2001@gmail.com>
2024-08-08 09:10:36 +0530

    Updating App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppLayout } from "./layout";
 import { HomePage } from "./pages/home";
 import { NotFoundPage } from "./pages/not-found";
+import { AdminArticlePage } from "./pages/AdminArticlePage";
+import { ArticlePage } from "./pages/ArticlePage";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <AppLayout>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/article" element={<ArticlePage />} />
+          <Route path="/admin-article" element={<AdminArticlePage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </AppLayout>
